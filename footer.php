b408d1bd3b41f805bd49d87843e754cd7b2d83b8
Bart <bart@meierij-it.nl>
2023-10-22 10:41:49 +0200

    Building out the boilerplate

---
+++ footer.php
@@ -0,0 +1,3 @@
+<?php
+// Loading the closing elements of the document
+include 'templates/partials/document-close.php';?>
---
+++ templates/partials/document-open.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <!-- Adding HTMX  -->
+  <script src="https://unpkg.com/htmx.org@1.9.6"></script>
+  <title>De Portfolio</title>
+</head>
