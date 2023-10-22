f85a937c2fe1d6133c9b6428d0a1dfba7a607af9
Bart <bart@meierij-it.nl>
2023-10-22 13:12:47 +0200

    Adding mobile nav using htmx

--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -11,8 +11,8 @@ $address = $_GET['address'];
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
-<!-- load alpine js -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js"></script>
+  <!-- load alpine js -->
+  <script src="//unpkg.com/alpinejs" defer></script>
   <!-- Adding HTMX  -->
   <script src="https://unpkg.com/htmx.org@1.9.6"></script>
   <link rel="icon" type="image/png" sizes="254x254" href="assets/img/favicon.png">
