07c5b16f2721d1d662c72c688c850aad809718ca
Bart <bart@meierij-it.nl>
2023-10-22 12:47:58 +0200

    Adding alpine js

--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -11,9 +11,12 @@ $address = $_GET['address'];
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
+<!-- load alpine js -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js"></script>
   <!-- Adding HTMX  -->
   <script src="https://unpkg.com/htmx.org@1.9.6"></script>
   <link rel="icon" type="image/png" sizes="254x254" href="assets/img/favicon.png">
+
 
   <title>De Portfolio</title>
 </head>
