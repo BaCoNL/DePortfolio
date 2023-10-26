bfdac91a5bfb9d8588fc4cbf138769652bc2972a
Bart <bart@meierij-it.nl>
2023-10-26 10:43:48 +0200

    Changing header title

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
     <div class="py-24 sm:py-32 lg:pb-40">
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-2xl text-center">
-          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich your online business</h1>
-          <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Track Assets On-chain with DePortfolio</h1>
+          <p class="mt-6 text-lg leading-8 text-gray-600">Your Secure Gateway to Comprehensive Decentralized Asset Management, Harnessing Blockchain for Real-time Portfolio Tracking and Insights.</p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
-            <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+            <a href="dashboard.php" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+<!--            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>-->
           </div>
         </div>
         <div class="mt-16 flow-root sm:mt-24">
