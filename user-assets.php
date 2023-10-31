cfbddeefcdb46d60981b3df34e1580b4b71dda7a
Bart <bart@meierij-it.nl>
2023-10-31 16:41:58 +0100

    Fix to assets page amount
    There where not displaying correctly

--- user-assets.php
+++ user-assets.php
@@ -1,4 +1,17 @@
-<?php include 'header.php'; ?>
+<?php include 'header.php';
+
+include_once 'app/functions/convertToDecimal.php';
+
+// load all tokens of user
+$tokens = json_decode(file_get_contents('https://datalayer.decommas.net/datalayer/api/v1/tokens/' . $address . '?limit=100?api-key=' . $deCommasApiKey . ''));
+$countTokens = count($tokens->result);
+if ($countTokens === 100) {
+  $countTokens = '100+';
+}
+
+
+$blockchains = json_decode(file_get_contents('https://datalayer.decommas.net/datalayer/api/v1/coins/' . $address . '?api-key=' . $deCommasApiKey . ''));
+?>
 
   <main>
 
@@ -25,38 +38,31 @@
                 </tr>
                 </thead>
                 <tbody class="bg-white">
-                <? // load all tokens of user
-                $tokens = json_decode(file_get_contents('https://datalayer.decommas.net/datalayer/api/v1/tokens/' . $address . '?limit=100?api-key=' . $deCommasApiKey . ''));
-                $countTokens = count($tokens->result);
-                if ($countTokens === 100) {
-                  $countTokens = '100+';
-                }
-                foreach ($tokens->result as $token):
-                  ?>
+                <? foreach ($blockchains->result as $blockchain): ?>
                   <tr class="even:bg-gray-50">
                     <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
-                      <? if ($token->logo_url): ?>
-                        <img src="<?= $token->logo_url; ?>" class="w-8 inline" alt="<?= $token->name; ?>">
+                      <? if ($blockchain->logo_url): ?>
+                        <img src="<?= $blockchain->logo_url; ?>" class="w-8 inline" alt="<?= $blockchain->name; ?>">
                       <? else: ?>
                         <img src="assets/img/unknown-token.jpeg" alt="Unknown Token"
                              class="w-8 inline">
                       <? endif; ?>
-                      <?= $token->name; ?>
+                      <?= $blockchain->name; ?>
                     </td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><?= $token->chain_name; ?></td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><?= $blockchain->chain_name; ?></td>
                     <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                       <?
-                  if ($token->actual_price):
-                      echo $token->actual_price;
-                  else:
-                      echo 'Unknown';
-                  endif;
-                  ?>
+                      if ($blockchain->actual_price):
+                        echo $blockchain->actual_price;
+                      else:
+                        echo 'Unknown';
+                      endif;
+                      ?>
                     </td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><?= $token->amount; ?></td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      <? if ($token->actual_price):
-                        echo $token->amount*$token->actual_price;
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><?= convertToDecimal($blockchain->amount, '18'); ?></td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">$
+                      <? if ($blockchain->actual_price):
+                        echo convertToDecimal($blockchain->amount, '18') * $blockchain->actual_price;
                       else:
                         echo 'Unknown';
                       endif; ?>
@@ -64,7 +70,47 @@
 
 
                   </tr>
+
                 <? endforeach; ?>
+                <?
+                foreach ($tokens->result as $token):
+
+                  if ($token->is_verified === true): ?>
+
+                    <tr class="even:bg-gray-50">
+                      <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+                        <? if ($token->logo_url): ?>
+                          <img src="<?= $token->logo_url; ?>" class="w-8 inline" alt="<?= $token->name; ?>">
+                        <? else: ?>
+                          <img src="assets/img/unknown-token.jpeg" alt="Unknown Token"
+                               class="w-8 inline">
+                        <? endif; ?>
+                        <?= $token->name; ?>
+                      </td>
+                      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><?= $token->chain_name; ?></td>
+                      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        <?
+                        if ($token->actual_price):
+                          echo $token->actual_price;
+                        else:
+                          echo 'Unknown';
+                        endif;
+                        ?>
+                      </td>
+                      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><?= sprintf('%.8f',floatval(convertToDecimal($token->amount, $token->decimals))); ?></td>
+                      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">$
+                        <? if ($token->actual_price):
+                          echo sprintf('%.8f',floatval(convertToDecimal($token->amount, $token->decimals) * $token->actual_price));
+                        else:
+                          echo 'Unknown';
+                        endif; ?>
+                      </td>
+
+
+                    </tr>
+                  <?
+                  endif;
+                endforeach; ?>
 
                 </tbody>
               </table>
